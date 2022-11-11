b48c2d980ec544577f6f406d7902bc65a126119f
sudinojkt <sudinojkt@hotmail.com>
2022-11-11 15:55:24 +0700

    add first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var map = L.map('map').setView([1.352083, 103.819836], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var circle = L.circle([1.352083, 103.819836], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([1.352083, 103.819836])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
