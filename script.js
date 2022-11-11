be04e0a50dd8a3f0c654c6361ee782ba61ff2996
sudinojkt <sudinojkt@hotmail.com>
2022-11-11 17:50:50 +0700

    remove h1

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var map = L.map('map').setView([1.352083, 103.819836], 11);
+var map = L.map('map').setView([1.352083, 103.819836], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//add marker 
+L.marker([1.352083, 103.819836]).addTo(map);
+.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+.openPopup();
+
+//bind marker popup
 var circle = L.circle([1.352083, 103.819836], {
-    color: 'red',
+    color: 'yellow',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 500
 }).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
 var popup = L.popup()
     .setLatLng([1.352083, 103.819836])
-    .setContent("I am a standalone popup.")
+    .setContent("I am at the center of Singapore.")
     .openOn(map);
